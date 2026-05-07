8aae556cae70fdfbbe4ae8d299a3caf46b9e4c4d
Abhinav <abhirealme55@gmail.com>
2026-05-07 18:29:47 +0530

    feat: add custom user model

---
+++ backend/apps/users/models.py
@@ -0,0 +1,61 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    class PlanChoices(models.TextChoices):
+        FREE = "FREE", "Free"
+        PRO = "PRO", "Pro"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    avatar_url = models.URLField(blank=True, null=True)
+
+    is_verified = models.BooleanField(default=False, help_text="Verified via OTP")
+    current_plan = models.CharField(
+        default=PlanChoices.FREE,
+        choices=PlanChoices.choices,
+        help_text="Pro subscription status",
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    # Hide AbstractUser's fields
+    username = None
+    first_name = None  # Removes the field
+    last_name = None  # Removes the field
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     #myapps
-    
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teeming_db',
+        'USER': 'teeming_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -105,3 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "users.User"
